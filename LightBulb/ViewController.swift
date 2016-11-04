45008a43600c22e8018adde368613d928625621d
Allen Soberano <allensoberano@gmail.com>
2016-11-04 04:10:27 -0700

    Lightbulb Done

--- LightBulb/ViewController.swift
+++ LightBulb/ViewController.swift
@@ -9,27 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var lightBulb: UIImageView!
-    
-    
-    override func viewDidLoad() {
-        
-        super.viewDidLoad()
-        
-        lightBulb.backgroundColor = UIColor.blue
-        
-    }
-    
-    
-    @IBAction func colorSelected(_ sender: UISegmentedControl) {
-        
-        print(sender.selectedSegmentIndex)
-        
-    }
-    
-    
 
-   
+    @IBOutlet weak var lightBulb: UIImageView!
+
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        lightBulb.backgroundColor = UIColor.red
+
+    }
+
+    @IBAction func colorSelected(_ sender: UISegmentedControl) {
+
+        print(sender.selectedSegmentIndex)
+
+        switch sender.selectedSegmentIndex {
+        case 0:
+            lightBulb.backgroundColor = UIColor.red
+        case 1:
+            lightBulb.backgroundColor = UIColor.yellow
+        case 2:
+            lightBulb.backgroundColor = UIColor.blue
+        case 3:
+            lightBulb.backgroundColor = UIColor.green
+        default:
+            print("Error some where")
+        }
+    }
 
 }
